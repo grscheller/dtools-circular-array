3257075ba1dad5ed7870c48a3623f8aed9793ebb
grscheller <geoffrey@scheller.com>
2024-08-15 18:02:19 -0600

    Giving grscheller README.md a common look

--- src/grscheller/circular_array/__init__.py
+++ src/grscheller/circular_array/__init__.py
@@ -14,7 +14,7 @@
 
 """### Package for an indexable circular array data structure."""
 
-__version__ = "3.4.0.1"
+__version__ = "3.4.0.2"
 __author__ = "Geoffrey R. Scheller"
 __copyright__ = "Copyright (c) 2023-2024 Geoffrey R. Scheller"
 __license__ = "Apache License 2.0"
